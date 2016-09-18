db84fbc51051caf7bd3220565eec1f26c1b89033
Joe Gregorio <jcgregorio@google.com>
2016-09-18 11:00:45 -0400

    Add login protection for referres page.

--- go/userve/ref.go
+++ go/userve/ref.go
@@ -1,30 +1,49 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"html/template"
 	"net/http"
 	"sort"
 
 	lru "github.com/hashicorp/golang-lru"
 	"github.com/skia-dev/glog"
+	"go.skia.org/infra/go/httputils"
 )
 
 type cacheEntry map[string]int
 
+const (
+	CLIENT_ID = "952643138919-jh0117ivtbqkc9njoh91csm7s465c4na.apps.googleusercontent.com"
+)
+
 var (
 	cache       *lru.Cache
 	refTemplate *template.Template
-	refSource   = `<!DOCTYPE html>
+	refSource   = fmt.Sprintf(`<!DOCTYPE html>
 <html>
 <head>
     <title></title>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=egde,chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="google-signin-scope" content="profile email">
+    <meta name="google-signin-client_id" content="%s">
+    <script src="https://apis.google.com/js/platform.js" async defer></script>
 </head>
 <body>
+  <div class="g-signin2" data-onsuccess="onSignIn" data-theme="dark"></div>
+    <script>
+      function onSignIn(googleUser) {
+        document.cookie = "id_token=" + googleUser.getAuthResponse().id_token;
+        if (!{{.LoggedIn}}) {
+          window.location.reload();
+        }
+      };
+    </script>
   <dl>
-  {{range .}}
+  {{range .Summary}}
     <dt>{{.Path}} - {{.Total}}</dt>
     <dd>
       <ul>
@@ -37,7 +56,8 @@ var (
   </dl>
 </body>
 </html>
-`
+`, CLIENT_ID)
+	client *http.Client
 )
 
 func incRef(path, referrer string) {
@@ -61,12 +81,47 @@ func incRef(path, referrer string) {
 
 func init() {
 	refTemplate = template.Must(template.New("ref").Parse(refSource))
+	client = httputils.NewTimeoutClient()
+}
+
+type Claims struct {
+	Mail string `json:"email"`
+	Aud  string `json:"aud"`
+}
+
+func loggedIn(r *http.Request) bool {
+	idtoken, err := r.Cookie("id_token")
+	if err != nil {
+		glog.Info("Cookie not set.")
+		return false
+	}
+	resp, err := client.Get(fmt.Sprintf("https://www.googleapis.com/oauth2/v3/tokeninfo?%s", idtoken))
+	if err != nil || resp.StatusCode != 200 {
+		glog.Errorf("Failed to validate idtoken: %#v %s", *resp, err)
+		return false
+	}
+	claims := Claims{}
+	if err := json.NewDecoder(resp.Body).Decode(&claims); err != nil {
+		glog.Errorf("Failed to decode claims: %s", err)
+		return false
+	}
+	// Check if aud is correct.
+	if claims.Aud != CLIENT_ID {
+		return false
+	}
+
+	return claims.Mail == "joe.gregorio@gmail.com"
 }
 
 type refSummary struct {
 	Path      string
 	Total     int
 	Referrers map[string]int
+}
+
+type refPageContext struct {
+	LoggedIn bool
+	Summary  []*refSummary
 }
 
 type refSummarySlice []*refSummary
@@ -79,31 +134,37 @@ func refHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	ikeys := cache.Keys()
 	summary := []*refSummary{}
-	for _, ik := range ikeys {
-		key := ik.(string)
-		irefs, ok := cache.Get(ik)
-		if !ok {
-			glog.Error("No cache hit?: %v", irefs)
-			continue
+	isLoggedIn := loggedIn(r)
+	if isLoggedIn {
+		for _, ik := range ikeys {
+			key := ik.(string)
+			irefs, ok := cache.Get(ik)
+			if !ok {
+				glog.Error("No cache hit?: %v", irefs)
+				continue
+			}
+			refs, ok := irefs.(cacheEntry)
+			if !ok {
+				glog.Error("Wrong thing in cache?: %v", irefs)
+				continue
+			}
+			total := 0
+			for _, n := range refs {
+				total += n
+			}
+			summary = append(summary, &refSummary{
+				Path:      key,
+				Total:     total,
+				Referrers: refs,
+			})
 		}
-		refs, ok := irefs.(cacheEntry)
-		if !ok {
-			glog.Error("Wrong thing in cache?: %v", irefs)
-			continue
-		}
-		total := 0
-		for _, n := range refs {
-			total += n
-		}
-		summary = append(summary, &refSummary{
-			Path:      key,
-			Total:     total,
-			Referrers: refs,
-		})
 	}
 	glog.Infof("%#v", summary)
 	sort.Sort(refSummarySlice(summary))
-	if err := refTemplate.Execute(w, summary); err != nil {
+	if err := refTemplate.Execute(w, refPageContext{
+		LoggedIn: isLoggedIn,
+		Summary:  summary,
+	}); err != nil {
 		glog.Errorf("Failed to render ref template: %s", err)
 	}
 }
